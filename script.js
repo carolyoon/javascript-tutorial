eb20dd013c26e68a3a548c352c924b2cb3143c00
carolyoon <carolyoon731@gmail.com>
2018-04-05 16:26:46 -0700

    Finish operators and if / else statement lectures

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Lecture: variables
+// Lecture: Variables
 /*
 var name = 'Caroline';
 console.log(name);
@@ -11,9 +11,6 @@ console.log(age);
 
 var fullAge = true;
 console.log(fullAge);
-*/
-
-//Lecture: variables 2
 
 var name = 'Caroline';
 var age = 29;
@@ -39,3 +36,76 @@ var lastName = prompt('What is the last name?');
 console.log(lastName);
 
 alert('name' + ' is a ' + age + ' year old ' + job + '. Is she married? ' + ' ' + isMarried);
+*/
+
+
+// Lecture: Operators
+/*
+var now = 2016
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+//2016 - 52 = 1964
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = 3 + 5 * 4 -6
+console.log(birthYear);
+console.log(ageJohn);
+*/
+
+// Lecture: If/Else Statements
+/*
+var name = 'Caroline';
+var age = 29;
+var isMarried = 'yes';
+
+if (isMarried === 'yes') {
+  console.log(name + ' is married!');
+} else {
+  console.log(name + 'is not married!');
+}
+
+isMarried = true;
+
+if(isMarried) {
+  console.log('Yes');
+} else {
+  console.log('No');
+}
+
+if(isMarried) {
+  console.log('Yes');
+}
+*/
+
+// Lecture:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
