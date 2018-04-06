d0c359a60a4410992522f3e67f6488a1f7ba7f50
carolyoon <carolyoon731@gmail.com>
2018-04-05 17:30:34 -0700

    Took notes on arrays

--- script.js
+++ script.js
@@ -149,9 +149,28 @@ var someFun = function(par) {
 }
 */
 
+// Lecture: Arrays
 
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
 
+console.log(names[2]);
+names[1] = 'Ben';
+console.log(names);
 
+var john = ['John', 'Smith', 1990, 'architect', 'false'];
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+john.indexOf('Smith');
+
+console.log(john);
+
+if (john.indexOf('teacher') === -1) {
+  console.log('John is not a teacher.');
+}
 
 
 
